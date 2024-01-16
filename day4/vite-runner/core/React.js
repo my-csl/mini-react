fa313325236db75e4559eecfd73fb38986c3ee8b
陈石林 <1826515410@qq.com>
2024-01-16 11:27:14 +0800

    feat(day4): 实现fiber&任务调度器（day2）

--- day4/vite-runner/core/React.js
+++ day4/vite-runner/core/React.js
@@ -22,21 +22,79 @@ function createTextNode(text) {
 }
 
 function render(el, container) {
-  const dom =
-    el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type);
+  nextWorkUnit = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  };
+}
 
-  Object.keys(el.props).forEach((key) => {
+function createDom(type) {
+  return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== 'children') {
-      dom[key] = el.props[key];
+      dom[key] = props[key];
     }
   });
-
-  el.props.children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.append(dom);
 }
+
+function initChildren(fiber) {
+  let prevChild = null;
+  fiber.props.children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      dom: null,
+      child: null,
+      sibling: null
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+
+function preformWorkOfUnit(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+    fiber.parent.dom.append(dom);
+
+    updateProps(dom, fiber.props);
+  }
+
+  initChildren(fiber);
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling;
+  }
+
+  return fiber.parent?.sibling;
+}
+
+let nextWorkUnit = null;
+function workLoop() {
+  let shouldYield = false;
+
+  while (!shouldYield && nextWorkUnit) {
+    nextWorkUnit = preformWorkOfUnit(nextWorkUnit);
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
 
 export default {
   render,
